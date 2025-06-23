4c7f476f48ee87f35f6372cc96b48eda6b70448a
victorroddev <javier.rod.dev@gmail.com>
2025-06-23 14:50:15 -0600

    update

--- sets.py
+++ sets.py
@@ -8,4 +8,20 @@ print(val)
 
 
 a_dict = { 'target': 12, 'non-target': 10, 'target': 9 }
-print(a_dict)
+print(list(a_dict))
+
+fcb = { 
+    "nombre": "Fútbol Club Barcelona",
+    "pais": "España",
+    "fundacion": 1899,
+    "entrenador": "Hansi Flick",
+    "estadio": "Spotify Camp Nou",
+    "titulos_liga": 27,
+    "titulos_champions": 5
+}
+
+print(sorted(fcb)) #Output: ['entrenador', 'estadio', 'fundacion', 'nombre', 'pais', 'titulos_champions', 'titulos_liga']
+print(list(fcb)) # ['nombre', 'pais', 'fundacion', 'entrenador', 'estadio', 'titulos_liga', 'titulos_champions']
+
+print(fcb['nombre'])
+
