13cd8e7217141cdc0961c7b0148e9a0c31b87fbb
victorroddev <javier.rod.dev@gmail.com>
2025-07-04 14:00:01 -0600

    Add exercises

--- t.js
+++ t.js
@@ -23,16 +23,33 @@ function luckyNumber(value) {
 }
 
 //console.log(luckyNumber(1441));
-
+/* version with else if
 function errorMessage(input) {
-    eval = Number(input);
-    if(isNaN(eval)){
-        return 'Must be a number';
-    }else if(eval instanceof Number){
-        return 'if works';
-    }else if(!eval){
-        return 'required field'
+    if(!input){
+        return 'Required field';
+    }else if(input){
+        let toEval = Number(input);
+        if(!toEval){
+            return 'Must be a number besides 0'
+        }
+        return ''
     }
 }
+*/
 
-console.log(errorMessage('abc'))
+function errorMessage(input) {
+    if(!input){
+        return 'Required field';
+    }{
+        let toEval = Number(input);
+        if(!toEval){
+            return 'Must be a number besides 0'
+    }
+        return ''
+    }
+    
+}
+
+console.log(errorMessage('123'))//''
+console.log(errorMessage('abc'))//Must be a number
+console.log(errorMessage(''))//Required field
